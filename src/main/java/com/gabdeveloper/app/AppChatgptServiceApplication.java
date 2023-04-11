7e08d65225affee34b15231304030127dc5199cb
Gabr1el94 <bielsystem64@gmail.com>
2023-04-10 21:23:20 -0300

    config initial ChatGPT

---
+++ src/main/java/com/gabdeveloper/app/AppChatgptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabdeveloper.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppChatgptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppChatgptServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabdeveloper/app/AppChatgptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabdeveloper.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppChatgptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
